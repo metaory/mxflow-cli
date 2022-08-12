7908258682523b25c1578361ab57fbb056aa5cc9
metaory <metaory@gmail.com>
2022-08-12 21:54:18 +0800

    added header

--- bin/index.js
+++ bin/index.js
@@ -8,31 +8,27 @@ import * as operations from '../lib/operations/index.js'
 
 import { checkIsGitDirectory } from '../lib/git.js'
 import { getConfig } from '../lib/config.js'
+import header from '../lib/header.js'
 
 $.verbose = argv.verbose ?? false
 
-async function main() {
-  const config = await getConfig()
-  console.log('@@@@', config, config?.trunk_branch_name)
+await header()
 
-  const isGitDirectory = await checkIsGitDirectory()
-  if (isGitDirectory === false) {
-    console.error(chalk.red('NOT A GIT REPOSITORY!'))
-    process.exit(1)
-  }
+const config = await getConfig()
+console.table({ config })
 
-  const { operation } = await listInput('operation', [
-    { name: 'commit', message: 'new commit', disabled: true },
-    { name: 'newWorkflow', message: 'start new workflow', disabled: true },
-    { name: 'checkConflict', message: 'check for conflicts' },
-    { name: 'PR_toLanding', message: 'create PR', disabled: true }
-  ])
-  await operations[operation]()
-
-  return 'bye!'
+const isGitDirectory = await checkIsGitDirectory()
+if (isGitDirectory === false) {
+  console.error(chalk.red('NOT A GIT REPOSITORY!'))
+  process.exit(1)
 }
 
-main()
-  .then(console.log)
-  .catch(console.error)
+const { operation } = await listInput('operation', [
+  { name: 'commit', message: 'new commit', disabled: true },
+  { name: 'newWorkflow', message: 'start new workflow', disabled: true },
+  { name: 'checkConflict', message: 'check for conflicts' },
+  { name: 'PR_toLanding', message: 'create PR', disabled: true }
+])
+
+await operations[operation](config)
 
