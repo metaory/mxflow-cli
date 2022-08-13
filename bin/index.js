9b524048ea9380fa9887135ea29054cf7b85a513
metaory <metaory@gmail.com>
2022-08-13 13:37:07 +0800

    refactors

--- bin/index.js
+++ bin/index.js
@@ -1,34 +1,23 @@
 #!/usr/bin/env node
-import {
-  stringInput,
-  listInput
-} from '../lib/prompts.js'
+console.clear``
 
+import { stringInput, listInput } from '../lib/prompts.js'
 import * as operations from '../lib/operations/index.js'
-
-import { checkIsGitDirectory } from '../lib/git.js'
 import { getConfig } from '../lib/config.js'
+import { catchNoGit } from '../lib/git.js'
 import header from '../lib/header.js'
+
+process.on('uncaughtException', () => {/* noop */})
 
 $.verbose = argv.verbose ?? false
 
 await header()
 
 const config = await getConfig()
-console.table({ config })
 
-const isGitDirectory = await checkIsGitDirectory()
-if (isGitDirectory === false) {
-  console.error(chalk.red('NOT A GIT REPOSITORY!'))
-  process.exit(1)
-}
+await catchNoGit()
 
-const { operation } = await listInput('operation', [
-  { name: 'commit', message: 'new commit', disabled: true },
-  { name: 'newWorkflow', message: 'start new workflow', disabled: true },
-  { name: 'checkConflict', message: 'check for conflicts' },
-  { name: 'PR_toLanding', message: 'create PR', disabled: true }
-])
+const { operation } = await listInput('operation', operations.list)
 
 await operations[operation](config)
 
