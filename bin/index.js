d2d8293014d27d7cd3292438f6b803e5f45459a2
metaory <metaory@gmail.com>
2022-08-24 22:16:58 +0800

    cleanups

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import 'zx/globals'
+import '../lib/helper.js'
+import '../lib/header.js'
 
 import { autocompleteInput } from '../lib/prompts.js'
 import * as operations from '../lib/operations/index.js'
 import { getConfig } from '../lib/config.js'
 import { catchNoGit, catchDirtyGit } from '../lib/catch.js'
-import '../lib/helper.js'
-import '../lib/header.js'
 
 process.on('uncaughtException', () => {})
 process.on('unhandledRejection', () => {})
