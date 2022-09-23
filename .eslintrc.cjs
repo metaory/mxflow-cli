82a0597d30c03e021ffdca6319b5bacb0ac0deae
metaory <metaory@gmail.com>
2022-09-23 13:54:58 +0800

    bugfix: local version missmatch check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
     fillTo: 'readonly',
     getModuleName: 'readonly',
     CWD: 'readonly',
+    YAML: 'readonly',
     PKG_PATH: 'readonly',
     PKG_NAME: 'readonly',
     runCommands: 'readonly',
