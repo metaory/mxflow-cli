19a64bf41c9b951a77649b11ec1c4b108534a8c2
metaory <metaory@gmail.com>
2023-06-29 20:43:05 +0800

    disabled experimental features

--- src/opts/index.js
+++ src/opts/index.js
@@ -1,9 +1,9 @@
-import conflictDetection from "./conflictDetection.js";
-import workflows from "../core/workflows.js";
-import checkoutBranch from "./checkout.js";
-import cherryPick from "./cherryPick.js";
-import worktree from "./worktree.js";
-import Config from "../lib/config.js";
+import conflictDetection from "./conflictDetection.js"
+import workflows from "../core/workflows.js"
+import checkoutBranch from "./checkout.js"
+import cherryPick from "./cherryPick.js"
+import worktree from "./worktree.js"
+import Config from "../lib/config.js"
 
 const list = () => [
   // {
@@ -20,24 +20,24 @@ const list = () => [
     name: "workflows",
     message: C.cyan(`${global.ICON.WORKFLOW} trigger workflow`),
   },
-  {
-    name: "checkoutBranch",
-    message: C.magenta(`${global.ICON.CHECKOUT} checkout branch`),
-    options: { base: "" },
-  },
-  {
-    name: "conflictDetection",
-    message: C.blue(`${global.ICON.CONFLICT} conflict detection`),
-  },
+  // {
+  //   name: "checkoutBranch",
+  //   message: C.magenta(`${global.ICON.CHECKOUT} checkout branch`),
+  //   options: { base: "" },
+  // },
+  // {
+  //   name: "conflictDetection",
+  //   message: C.blue(`${global.ICON.CONFLICT} conflict detection`),
+  // },
   // {
   //   name: 'cherryPick',
   //   disabled: true,
   //   message: C.yellow('◈ cherry pick')
   // },
-  {
-    name: "worktree",
-    message: C.green(`${global.ICON.WORKTREE} worktree`), // ⭓
-  },
+  // {
+  //   name: "worktree",
+  //   message: C.green(`${global.ICON.WORKTREE} worktree`), // ⭓
+  // },
   {
     name: "reset",
     message: C.red(`${global.ICON.FAIL} reset system config`),
@@ -45,8 +45,8 @@ const list = () => [
   // { name: 'cherryPick', message: 'interactive cherry pick', disabled: true },
   // { name: 'PR_toLanding', message: 'create pull request', disabled: true },
   // { name: 'install_husky_hook', message: 'install husky hook', disabled: true }
-];
-const reset = Config.reset;
+]
+const reset = Config.reset
 
 export {
   conflictDetection,
@@ -56,4 +56,4 @@ export {
   worktree,
   list,
   reset,
-};
+}
